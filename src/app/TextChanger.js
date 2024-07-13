3bb4d7a4a0af9b2376d2fe7cda103b732a2ad00f
rnargot <margot.murvihill@gmail.com>
2024-07-12 18:52:57 -0700

    blind mobile attempt

--- src/app/TextChanger.js
+++ src/app/TextChanger.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './TextChanger.css';
 
 const TextChanger = () => {
-  const strings = [" glock fishing", "being sexy", "giggling", "on aurora boatealis", "being so brave", "finding boy wallet", "scissoring", "spilling tea", "guts deep in nat's bidet"];
+  const strings = [" glock fishing", "being sexy", "giggling", "on aurora boatealis", "being so brave", "finding boy wallet", "scissoring", "spilling tea", "guts deep in nat's bidet", "mechanical bull riding", "doing flips", "omw to tacoma", "slapping sunburns"];
   const [currentText, setCurrentText] = useState("");
 
   useEffect(() => {
